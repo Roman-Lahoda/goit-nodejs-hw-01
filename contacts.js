05939698f4cdc4709f9a3de661133464c22d9070
Roman-Lahoda <lagoda9515@gmail.com>
2021-12-19 20:18:42 +0200

    hw

--- contacts.js
+++ contacts.js
@@ -17,14 +17,28 @@ const listContacts = async () => {
 
 const getContactById = async (contactId) => {
   const contacts = await readContent();
-  const [foundContact] = contacts.filter((contact) => contact.id === contactId);
+
+  const foundContact = contacts.reduce((acc, contact) => {
+    if (contact.id === contactId) {
+      acc = contact;
+      return acc;
+    }
+    return acc;
+  });
 
   return foundContact;
 };
 
 const removeContact = async (contactId) => {
   const contacts = await readContent();
-  const updateContacts = contacts.filter((contact) => contact.id !== contactId);
+
+  const updateContacts = contacts.reduce((array, contact) => {
+    if (contact.id !== contactId) {
+      array.push(contact);
+    }
+    return array;
+  }, []);
+
   await fs.writeFile(
     path.join(__dirname, "db", "contacts.json"),
     JSON.stringify(updateContacts, null, 2)
@@ -36,6 +50,7 @@ const addContact = async (name, email, phone) => {
   const contacts = await readContent();
   const newContact = { name, email, phone, id: crypto.randomUUID() };
   contacts.push(newContact);
+
   await fs.writeFile(
     path.join(__dirname, "db", "contacts.json"),
     JSON.stringify(contacts, null, 2)
@@ -45,13 +60,16 @@ const addContact = async (name, email, phone) => {
 
 const updateContact = async (id, name, email, phone) => {
   const contacts = await readContent();
-  const updatedContactsList = contacts.map((contact) => {
-    if (contact.id === id) {
-      return (contact = { id, name, email, phone });
+
+  const updatedContactsList = contacts.reduce((acc, contact) => {
+    if (contact.id !== id) {
+      acc.push(contact);
     } else {
-      return contact;
+      acc.push({ id, name, email, phone });
     }
-  });
+    return acc;
+  }, []);
+
   await fs.writeFile(
     path.join(__dirname, "db", "contacts.json"),
     JSON.stringify(updatedContactsList, null, 2)
